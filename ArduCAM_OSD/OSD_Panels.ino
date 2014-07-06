4988c3d2ac66b6859e0e1402fdc489876e715a6e
Rob G <bob01@rogers.com>
2014-07-06 03:39:44 -0400

    OSD-16: MAV_TYPE - support additional multi-rotor types (APM pre v3.2 reported all as QUADCOPTER)

--- ArduCAM_OSD/OSD_Panels.ino
+++ ArduCAM_OSD/OSD_Panels.ino
@@ -927,31 +927,37 @@ void panFlightMode(int first_col, int first_line){
     osd.openPanel();
     //char c1 = 0xE0 ;//"; char c2; char c3; char c4; char c5; 
     char* mode_str="";
-    if (apm_mav_type == 2){ //ArduCopter MultiRotor or ArduCopter Heli
-        if (osd_mode == 0) mode_str = "stab"; //Stabilize
-        else if (osd_mode == 1) mode_str = "acro"; //Acrobatic
-        else if (osd_mode == 2) mode_str = "alth"; //Alt Hold
-        else if (osd_mode == 3) mode_str = "auto"; //Auto
-        else if (osd_mode == 4) mode_str = "guid"; //Guided
-        else if (osd_mode == 5) mode_str = "loit"; //Loiter
-        else if (osd_mode == 6) mode_str = "retl"; //Return to Launch
-        else if (osd_mode == 7) mode_str = "circ"; //Circle
-        else if (osd_mode == 8) mode_str = "posi"; //Position
-        else if (osd_mode == 9) mode_str = "land"; //Land
-        else if (osd_mode == 10) mode_str = "oflo"; //OF_Loiter
-        else if (osd_mode == 11) mode_str = "drif"; //Drift
-        else if (osd_mode == 15) mode_str = "tune"; //Auto tune
-        else if (osd_mode == 16) mode_str = "hybr"; //Hybrid
-    } else if(apm_mav_type == 1){ //ArduPlane
-        if (osd_mode == 0) mode_str = "manu"; //Manual
-        else if (osd_mode == 1) mode_str = "circ"; //CIRCLE
-        else if (osd_mode == 2) mode_str = "stab"; //Stabilize
-        else if (osd_mode == 5) mode_str = "fbwa"; //FLY_BY_WIRE_A
-        else if (osd_mode == 6) mode_str = "fbwb"; //FLY_BY_WIRE_B
-        else if (osd_mode == 10) mode_str = "auto"; //AUTO
-        else if (osd_mode == 11) mode_str = "retl"; //Return to Launch
-        else if (osd_mode == 12) mode_str = "loit"; //Loiter
-        else if (osd_mode == 15) mode_str = "guid"; //GUIDED
+    switch (apm_mav_type) { //ArduCopter MultiRotor or ArduCopter Heli
+        case MAV_TYPE_TRICOPTER:
+        case MAV_TYPE_QUADROTOR:
+        case MAV_TYPE_HEXAROTOR:
+        case MAV_TYPE_OCTOROTOR:
+            if (osd_mode == 0) mode_str = "stab"; //Stabilize
+            else if (osd_mode == 1) mode_str = "acro"; //Acrobatic
+            else if (osd_mode == 2) mode_str = "alth"; //Alt Hold
+            else if (osd_mode == 3) mode_str = "auto"; //Auto
+            else if (osd_mode == 4) mode_str = "guid"; //Guided
+            else if (osd_mode == 5) mode_str = "loit"; //Loiter
+            else if (osd_mode == 6) mode_str = "retl"; //Return to Launch
+            else if (osd_mode == 7) mode_str = "circ"; //Circle
+            else if (osd_mode == 8) mode_str = "posi"; //Position
+            else if (osd_mode == 9) mode_str = "land"; //Land
+            else if (osd_mode == 10) mode_str = "oflo"; //OF_Loiter
+            else if (osd_mode == 11) mode_str = "drif"; //Drift
+            else if (osd_mode == 15) mode_str = "tune"; //Auto tune
+            else if (osd_mode == 16) mode_str = "hybr"; //Hybrid
+            break;
+        case MAV_TYPE_FIXED_WING:
+            if (osd_mode == 0) mode_str = "manu"; //Manual
+            else if (osd_mode == 1) mode_str = "circ"; //CIRCLE
+            else if (osd_mode == 2) mode_str = "stab"; //Stabilize
+            else if (osd_mode == 5) mode_str = "fbwa"; //FLY_BY_WIRE_A
+            else if (osd_mode == 6) mode_str = "fbwb"; //FLY_BY_WIRE_B
+            else if (osd_mode == 10) mode_str = "auto"; //AUTO
+            else if (osd_mode == 11) mode_str = "retl"; //Return to Launch
+            else if (osd_mode == 12) mode_str = "loit"; //Loiter
+            else if (osd_mode == 15) mode_str = "guid"; //GUIDED
+            break;
     }
     osd.printf("%c%s", 0xE0, mode_str);
     osd.closePanel();
